edbade99ad421a8e17aa53ca746f907ecdef0970
Phumthawan <knzphumthawan@gmail.com>
2019-06-05 04:34:44 +0700

    authenticate user use email and phone

--- server.js
+++ server.js
@@ -1,10 +1,22 @@
 const next = require('next');
 const express = require('express');
+const axios = require('axios');
 
+const AUTH_USER_TYPE = "authenticated";
 const dev = process.env.NODE_ENV !== 'production';
 const port = process.env.PORT || 3000;
 const app = next({ dev });
 const handle = app.getRequestHandler();
+
+const authenticate = async (email, password) => {
+    const { data } = await axios.get('http://jsonplaceholder.typicode.com/users')
+
+    return data.find(user => {
+        if (user.email === email && user.phone === password) {
+            return user
+        }
+    });
+}
 
 app.prepare().then(() => {
     const server = express();
@@ -12,13 +24,18 @@ app.prepare().then(() => {
     //middleware
     server.use(express.json());
 
-    server.post('/api/login', (req, res) => {
+    server.post('/api/login', async (req, res) => {
         const { email, password } = req.body;
-        res.json({
-            email,
-            password,
-            success: true
-        })
+        const user = await authenticate(email, password)
+        if (!user) {
+            return res.status(403).send('Invalid Email or Password ');
+        }
+        const userData = {
+            name: user.name,
+            email: user.email,
+            type: AUTH_USER_TYPE
+        }
+        res.json(userData);
     })
 
     server.get('*', (req, res) => {
