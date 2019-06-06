44d4e010c0402aba5daaddefa3a050d773aba518
Phumthawan <knzphumthawan@gmail.com>
2019-06-07 06:05:27 +0700

    Persisting Session Client and Server

--- pages/index.js
+++ pages/index.js
@@ -1,14 +1,18 @@
 import Layout from '../components/Layout';
 import Link from 'next/link';
+import {authInitialProps} from "../lib/auth";
 
-const index = () => {
+const index = (props) => {
+    // console.log('index props',props);
     return(
-        <Layout title="Home">
+        <Layout title="Home" {...props}>
             <Link href="/profile">
                 <a>Go to profile</a>
             </Link>
         </Layout>
     )
-}
+};
+
+index.getInitialProps = authInitialProps();
 
 export  default index;
